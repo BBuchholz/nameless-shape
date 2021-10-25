d0bab60df0d116238c8047910bc25a90df9ebca7
ENTOMDT <entomdt@gmail.com>
2021-10-25 12:19:46 -0500

    Update netlify settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
+const netlify = require('@sveltejs/adapter-netlify')
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
 };
 
 export default config;
+	
